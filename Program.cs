7dbc8dd7a2329b91538557e2e121ab8939f47d3c
Kirill <kiril-shitikov@yandex.ru>
2023-05-02 13:20:47 +0300

    docs: first task done

--- Program.cs
+++ Program.cs
@@ -5,3 +5,21 @@
 // 5 -2 33 -2
 // 77 3 8 1
 
+// Console.WriteLine("Введите число №1");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число №2");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int[rows, columns];
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLongLength(1); j++)
+//     {
+//         array[i, j] = new Random().Next(0, 10);
+//         Console.Write(array[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
