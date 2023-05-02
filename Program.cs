6b2baa4e8559e549c8de0e7404ef2e2aa9202464
Kirill <kiril-shitikov@yandex.ru>
2023-05-02 13:43:06 +0300

    docs: HW 1st task

--- Program.cs
+++ Program.cs
@@ -23,3 +23,30 @@
 //     Console.WriteLine();
 // }
 
+
+
+// Homework
+
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Console.WriteLine("Введите число №1");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число №2");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// double[,] array = new double[rows, columns];
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLongLength(1); j++)
+//     {
+//         array[i, j] = new Random().Next(-99, 99) / 10.0;
+//         Console.Write(array[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
